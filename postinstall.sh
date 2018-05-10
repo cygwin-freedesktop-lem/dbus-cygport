8555ccc5be364220d9a33dc6d03fe13f4b562040
Yaakov Selkowitz <yselkowi@redhat.com>
2018-05-09 19:08:33 -0500

    dbus 1.12.8

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,13 @@
 mkdir -p /var/lib/dbus
 /usr/bin/dbus-uuidgen --ensure
+
+if [ -x /usr/bin/xmlcatalog ] ; then
+	/usr/bin/xmlcatalog --noout --add "system" \
+		"http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd" \
+		/usr/share/xml/dbus-1/busconfig.dtd \
+		/etc/xml/catalog
+	/usr/bin/xmlcatalog --noout --add "system" \
+		"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd" \
+		/usr/share/xml/dbus-1/introspect.dtd \
+		/etc/xml/catalog
+fi
--- preremove.sh
+++ preremove.sh
@@ -2,3 +2,12 @@ if /usr/bin/cygrunsrv -Q messagebus &>/dev/null
 then
 	/usr/bin/cygrunsrv -E messagebus
 fi
+
+if [ -x /usr/bin/xmlcatalog ] ; then
+	/usr/bin/xmlcatalog --noout --del \
+		/usr/share/xml/dbus-1/busconfig.dtd \
+		/etc/xml/catalog
+	/usr/bin/xmlcatalog --noout --del \
+		/usr/share/xml/dbus-1/introspect.dtd \
+		/etc/xml/catalog
+fi
